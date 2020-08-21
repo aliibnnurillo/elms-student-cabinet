02856627b68a26a2cb6e3210a4f91ad962433026
csexplorer <salimibnusmon@gmail.com>
2020-08-21 07:31:05 +0500

    some auth page styles refactored

--- src/pages/auth/ForgotPassword.js
+++ src/pages/auth/ForgotPassword.js
@@ -9,29 +9,31 @@ const ForgotPassword = () => {
     <div className="auth  empty-page">
       <Row gutter={40}>
         <Col span={7} className="auth-left">
-          <Avatar src="/assets/userimg.jpg" alt="Alisher Saidov" size={112} />
+          <div className="forgot-pass">
+            <Avatar src="/assets/userimg.jpg" alt="Alisher Saidov" size={112} />
 
-          <Form name="normal_login" className="login-form">
-            <h1>{t("Parolni qayta tiklash")}</h1>
-            <Form.Item name="username">
-              <Input
-                prefix={<UserOutlined className="site-form-item-icon" />}
-                placeholder={t("Login yoki email")}
-              />
-            </Form.Item>
+            <Form name="normal_login" className="login-form">
+              <h1>{t("Parolni qayta tiklash")}</h1>
+              <Form.Item name="username">
+                <Input
+                  prefix={<UserOutlined className="site-form-item-icon" />}
+                  placeholder={t("Login yoki email")}
+                />
+              </Form.Item>
 
-            <Form.Item>
-              <Button
-                type="primary"
-                htmlType="submit"
-                className="login-form-button"
-                block
-                href="reset-password"
-              >
-                {t("Tiklash")}
-              </Button>
-            </Form.Item>
-          </Form>
+              <Form.Item>
+                <Button
+                  type="primary"
+                  htmlType="submit"
+                  className="login-form-button"
+                  block
+                  href="reset-password"
+                >
+                  {t("Tiklash")}
+                </Button>
+              </Form.Item>
+            </Form>
+          </div>
         </Col>
         <Col span={17} className="auth-right"></Col>
       </Row>
